f2d793232b16142e80b60d89a6f29e12c645a1c6
h1tch01 <33521588+h1tch01@users.noreply.github.com>
2024-02-16 16:13:11 -0600

    Update dnsreport.py
    Clear up and update code.
    Add threading.

--- dnsreport.py
+++ dnsreport.py
@@ -1,99 +1,114 @@
 #!/usr/bin/env python3
-# 
+#
 # dnsreport.py
 # Brian Hitchcock (h1tch)
-# 2020/09/15
+# 2020/09/15 - Initial
+# 2024/02/16 - Modernize and add threading
 # Python3 DNS lookup - Formats in CSV for Reports
 #EXAMPLE
-#python3 dnsreport.py --domain google.com --subdomain subdomains-100.txt
+#python3 dnsreport.py google.com subdomains-100.txt
+# requirements
 # pip3 install dnspython
-#
+
+import threading
 import dns.resolver
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--domain', help="domain name to test example domain.com", required=True)
-parser.add_argument('--subdomain',default="subdomains-100.txt", help="subdomain wordlist to brute")
-args = parser.parse_args()
+def resolve_subdomain(subdomain, domain, Output):
+  try:
+    answers = dns.resolver.resolve(subdomain + "." + domain)
+    answers.timeout = 2
+    answers.lifetime = 2
+    for answer in answers:
+      print(f"{subdomain}.{domain},A,{answer.to_text() }")
+      with open(Output, 'a') as file:
+        file.write(f"{subdomain}.{domain},A,{answer.to_text() }")
+        file.write("\n")
+  except dns.resolver.NXDOMAIN:
+    pass  # Subdomain not found
+  except dns.resolver.NoAnswer:
+    pass  # No answer for the query
+  return
 
-domain = args.domain
-SubDomain = args.subdomain
-# define file name to save output and open for write
-Output = "DNS-" + domain + ".csv"
-OutPutFile = open(Output, "w")
+def main():
+  parser = argparse.ArgumentParser(description="Perform DNS queries using a text file of subdomains")
+  parser.add_argument("domain", help="The target domain")
+  parser.add_argument("subdomains_file",default="subdomains-100.txt", help="The file containing subdomains")
+  args = parser.parse_args()
+  Output = "DNS-" + args.domain + ".csv"
+      
+  print ("\n***-=Writing output to file=-***   " + Output + "\n")
+  # NS Lookup
+  resultN = dns.resolver.resolve(args.domain, 'NS')
+  for ipval in resultN:
+          print(args.domain +",NS," + ipval.to_text())
+          with open(Output, 'w') as file:
+              file.write(args.domain +",NS," + ipval.to_text() + "\n")
 
+  # MX Lookup
+  resultM = dns.resolver.resolve(args.domain,'MX')
+  for ipval in resultM:
+          print(args.domain +",MX," + ipval.to_text())
+          with open(Output, 'a') as file:
+            file.write(args.domain +",MX," + ipval.to_text() + "\n")
 
-# does the actual lookups and returns results
-def dns_lookup(record,Rtype):
-        try:
-            result = dns.resolver.resolve(record, Rtype)
-        except:
-        	#print (result)
-        	# ignore errors aka record doesnt exsist
-        	return []
+  # TXT Lookup
+  resultT = dns.resolver.resolve(args.domain,'TXT')
+  for ipval in resultT:
+          print(args.domain +",TXT," + ipval.to_text())
+          with open(Output, 'a') as file:
+            file.write(args.domain +",TXT," + ipval.to_text() + "\n")
 
-        return result 
+  # domain only A Lookup
+  resultA = dns.resolver.resolve(args.domain,'A')
+  for ipval in resultA:
+          print(args.domain +",A," + ipval.to_text())
+          with open(Output, 'a') as file:
+            file.write(args.domain +",A," + ipval.to_text() + "\n")
 
+  # domain only CNAME Lookup
+  try:
+    resultC = dns.resolver.resolve(args.domain,'CNAME')
+    for ipval in resultC:
+            print(args.domain +",CNAME," + ipval.to_text())
+            with open(Output, 'a') as file:
+                file.write(args.domain +",CNAME," + ipval.to_text() + "\n")
+  except dns.resolver.NXDOMAIN:
+    pass  # Subdomain not found
+  except dns.resolver.NoAnswer:
+    pass
 
-print ("\n***-=Writing output to file=-***   " + Output + "\n")
-# NS Lookup
-resultN = dns_lookup(domain,'NS')
-for ipval in resultN:
-	print(domain +",NS," + ipval.to_text())
-	OutPutFile.write(domain +",NS," + ipval.to_text() + "\n")
+  WildCardIP = "999.999.999.999"
+  WildCardTrue = "0"
+  # Wildcard Check
+  Wdomain = "klsdfioer893jskdf89IDf893skd." + args.domain
+  try:
+    resultW = dns.resolver.resolve(Wdomain,'A')
+    if resultW:
+           for ipval in resultW:
+                   WildCardIP = ipval.to_text()
+                   print ("*." + args.domain + ",A," + WildCardIP)
+                   WildCardTrue = "1"
+                   with open(Output, 'a') as file:
+                     file.write("*." + args.domain + ",A," + WildCardIP  + "\n")
+  except dns.resolver.NXDOMAIN:
+    pass  # Subdomain not found
+  except dns.resolver.NoAnswer:
+    pass
 
-# MX Lookup
-resultM = dns_lookup(domain,'MX')
-for ipval in resultM:
-	print(domain +",MX," + ipval.to_text())
-	OutPutFile.write(domain +",MX," + ipval.to_text() + "\n")
+    # open subdomains file passed as an arg
+  with open(args.subdomains_file, "r") as file:
+    subdomains = file.read().splitlines()
+    # brute force A record lookup if Wildcard is not in use
+  if WildCardTrue == "0":
+    threads = []
+    for subdomain in subdomains:
+     thread = threading.Thread(target=resolve_subdomain, args=(subdomain, args.domain, Output))
+     threads.append(thread)
+     thread.start()
 
-# TXT Lookup
-resultT = dns_lookup(domain,'TXT')
-for ipval in resultT:
-	print(domain +",TXT," + ipval.to_text())
-	OutPutFile.write(domain +",TXT," + ipval.to_text() + "\n")
+    for thread in threads:
+     thread.join()
 
-# domain only A Lookup
-resultA = dns_lookup(domain,'A')
-for ipval in resultA:
-	print(domain +",A," + ipval.to_text())
-	OutPutFile.write(domain +",A," + ipval.to_text() + "\n")
-
-# domain only CNAME Lookup
-resultC = dns_lookup(domain,'CNAME')
-for ipval in resultC:
-	print(domain +",CNAME," + ipval.to_text())
-	OutPutFile.write(domain +",CNAME," + ipval.to_text() + "\n")
-	
-WildCardIP = "999.999.999.999"
-# Wildcard Check
-Wdomain = "klsdfioer893jskdf89IDf893skd." + domain
-resultW = dns_lookup(Wdomain,'A')
-if resultW:
-	for ipval in resultW:
-		WildCardIP = ipval.to_text()
-		print ("*." + domain + ",A," + WildCardIP)
-		OutPutFile.write("*." + domain + ",A," + WildCardIP  + "\n")
-
-
-with open(SubDomain) as fp:
-	for cnt, line in enumerate(fp):
-		Trecord = line.strip() + "." + domain
-
-
-# A  from subdomain list Record brute
-		resultA = dns_lookup(Trecord,'A')
-		for ipval in resultA:
-			if ipval.to_text() != WildCardIP:
-				print(line.strip() + "." + domain +",A," + ipval.to_text())
-				OutPutFile.write(line.strip() + "." + domain +",A," + ipval.to_text() + "\n")
-# Cname from subdomain list record brute
-		resultC = dns_lookup(Trecord,'CNAME')
-		for ipval in resultC:
-			if ipval.to_text() != WildCardIP:
-				print(line.strip() + "." + domain +",CNAME," + ipval.to_text())
-				OutPutFile.write(line.strip() + "." + domain +",CNAME," + ipval.to_text() + "\n")
-
-
-OutPutFile.close()
+if __name__ == "__main__":
+  main()
